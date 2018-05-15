24b4cd5dd1a9814ff0f759ef0483afbd302082b8
yulin650 <39178652+yulin650@users.noreply.github.com>
2018-05-15 18:33:44 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int i,j;
+	for(i=1;i<11;i++)
+	{
+		printf("%d ",i);
+	}
+	printf("\n");
+	for(j=10;j>0;j--)
+	{
+		printf("%d ",j);
+	}
+}
